9489479016680d49a79b5b303f469cc6ded8d9a3
Raj parmar <75210595+parmarraj999@users.noreply.github.com>
2022-08-25 15:35:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// make menu systme 
+const nav2 = document.querySelector(".nav-2");
+const menuBtn = document.querySelector(".menu_btn");
+
+menuBtn.addEventListener("click", openNav);
+function openNav(){
+    nav2.style.display = "flex";
+    
+}
